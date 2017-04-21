2440e7b6c87a0dbef31d936fc7a0be3eac904e20
Daniil Sorokin <dsorokin@envionsoftware.com>
2017-04-21 13:57:12 +0300

    added css loader

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import style from './App.css';
+
+console.log(style);
+
 export default class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
+     <div style={{textAlign: 'center'}} className={style.title}>
         <h1>Hello World</h1>
       </div>);
   }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
   inject: 'body'
-})
+});
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './client/index.js',
   output: {
@@ -14,8 +17,13 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.css/,
+        loader: ExtractTextPlugin.extract('css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'),
+
+      }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [HtmlWebpackPluginConfig, new ExtractTextPlugin("styles.css")]
 }
